7911a80302a7f9c1cca71f4651846adba3a8010c
Jamaril24 <jamarlopez24@g.crookcountyschools.org>
2021-10-28 09:35:33 -0700

    finished

---
+++ script.js
@@ -0,0 +1,19 @@
+'use strict'
+
+console.log('hello')
+
+if(7<4){
+    console.log('7 is less than 4')
+}
+else{
+    console.log('Not correct 7 is not less than 4')
+};
+if(9>2){
+    console.log('Correct 9 is greater than 2')
+}
+else if (3<4){
+    console.log('4 is greater than 3')
+}
+else{
+    console.log('Not correct')
+}
